4fed7155d3f414387d31e468c478488ec86d322c
Nexterbyby <jenseichenberger@gmail.com>
2021-05-19 11:18:51 +0200

    Inital commit.

---
+++ src/main/java/ch/bbw/je/Main.java
@@ -0,0 +1,12 @@
+package ch.bbw.je;
+
+/**
+ * @author Nexterbyby
+ * @version 0.0.1 19.05.2021
+ * @since 0.0.1 19.05.2021
+ */
+public class Main {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
