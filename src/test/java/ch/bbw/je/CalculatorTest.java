5411a29fc871b4a4497e7c43b9504ab6164bbc47
Nexterbyby <jenseichenberger@gmail.com>
2021-05-26 11:51:09 +0200

    Add CalculatorTest.java

---
+++ src/test/java/ch/bbw/je/CalculatorTest.java
@@ -0,0 +1,18 @@
+package ch.bbw.je;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Nexterbyby
+ * @version 0.0.1 26.05.2021
+ * @since 0.0.1 26.05.2021
+ */
+public class CalculatorTest {
+    Calculator c;
+
+    @Before
+    public void initialize(){
+        c = new Calculator();
+    }
+}
